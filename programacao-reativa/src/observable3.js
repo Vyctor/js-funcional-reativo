dac9786d2e042b39647cec12d0f8e3e5c5ce28eb
Vyctor <dev.vyctor@gmail.com>
2021-01-10 19:49:07 -0300

    desafio observable

--- programacao-reativa/src/observable3.js
+++ programacao-reativa/src/observable3.js
@@ -1,18 +1,18 @@
 const { Observable, noop } = require("rxjs");
 
 function imprimirEntre(min, max) {
-  if (min > max) [min, max] = [max, min];
-
   return new Observable((observer) => {
-    for (let index = min + 1; index < max; index++) {
-      observer.next(index);
-    }
+    Array(max - min + 1)
+      .fill()
+      .map((_, i) => {
+        observer.next(min + i);
+      });
     observer.complete();
   });
 }
 
-imprimirEntre(1, 10).subscribe(
-  (number) => console.log(number),
+imprimirEntre(4, 10).subscribe(
+  (number) => console.log("num =", number),
   noop,
-  () => console.log("FIM")
+  () => console.log("FIM!")
 );
